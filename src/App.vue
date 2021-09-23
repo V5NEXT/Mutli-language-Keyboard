ca307cc2acdd9e21d367c32018582e15a275b788
Vishnu Viswambharan <vviswambharan@DRGT.systems>
2021-09-23 14:11:05 +0200

    Addded flags

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
   <div id="app">
-<select class="form-select form-select-lg mb-3 form-control" aria-label=".form-select-lg example" name="LeaveType" v-model="leaveType" @change="onChangeLang()">
-  <option value="english">English</option>
-  <option value="german">German</option>
-  <option value="french">French</option>
-  <option value="spanish">Spanish</option>
-    <option value="chinese">Chinese</option>
-
-</select>
+    <select
+      class="form-select form-select-lg mb-3 form-control"
+      aria-label=".form-select-lg example"
+      name="LeaveType"
+      v-model="leaveType"
+      @change="onChangeLang()"
+    >
+      <option value="english">English</option>
+      <option value="german">German</option>
+      <option value="french">French</option>
+      <option value="spanish">Spanish</option>
+      <option value="chinese">Chinese</option>
+    </select>
 
     <input
       :value="input"
       class="input"
       @input="onInputChange"
       placeholder="Tap on the virtual keyboard to start"
-    >
-    <SimpleKeyboard @onChange="onChange" @onKeyPress="onKeyPress" :input="input" :language="leaveType" ref="childComponent"/>
+    />
+    <SimpleKeyboard
+      @onChange="onChange"
+      @onKeyPress="onKeyPress"
+      :input="input"
+      :language="leaveType"
+      ref="childComponent"
+    />
   </div>
 </template>
 
@@ -26,30 +37,29 @@ import "./App.css";
 export default {
   name: "App",
   components: {
-    SimpleKeyboard
+    SimpleKeyboard,
   },
 
   data: () => ({
     input: "",
-    leaveType: 'english',
-
+    leaveType: "english",
   }),
   methods: {
     onChange(input) {
       this.input = input;
+      console.log("Value", this.input);
     },
-       onChangeLang() {
-            console.log('The new value is: ', this.leaveType)
-            this.$refs.childComponent.setValue(this.leaveType)
-        },
+    onChangeLang() {
+      console.log("The new value is: ", this.leaveType);
+      this.$refs.childComponent.setValue(this.leaveType);
+    },
     onKeyPress(button) {
-      
       // console.log("button", button);
     },
     onInputChange(input) {
       this.input = input.target.value;
-    }
-  }
+    },
+  },
 };
 </script>
 
